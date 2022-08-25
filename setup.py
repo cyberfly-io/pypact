53596c0753efbbd849889571628203eaa00c98ed
Syed Abuthahir <abu@Syeds-MBP.domain.name>
2022-08-25 09:09:18 +0530

    fix verify function and add verify example

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
     name='pypact-lang',
-    version='0.3.3',
+    version='0.3.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
